b9e52b7ddb3439b9b5ca449179267c2ef46f0116
Federico <jpcoscolluela@up.edu.ph>
2022-05-22 09:36:04 +0800

    Created Profile Class for Admin

--- markitten/markitten_app/apps.py
+++ markitten/markitten_app/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class MarkittenAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'markitten_app'
+
+    #automatically create profile of registered user by invoking signals.py
+    # def ready(self):
+    #     import markitten_app.signals
--- markitten/markitten_app/urls.py
+++ markitten/markitten_app/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name="home"),
@@ -15,3 +18,6 @@ urlpatterns = [
     path('faq/', views.faq, name="faq"),
     path('about/', views.about, name="about"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
