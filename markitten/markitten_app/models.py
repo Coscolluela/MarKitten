25c28bb91c8bc80edb300beb57c3ad6e6753a6dd
Federico <jpcoscolluela@up.edu.ph>
2022-05-22 10:07:52 +0800

    Added more attribute in Model Profile

--- markitten/markitten_app/models.py
+++ markitten/markitten_app/models.py
@@ -7,6 +7,18 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     image = models.ImageField(default='default.png', upload_to='profile_pics')
+    sex = models.CharField(max_length=100, default='Male/Female')
+    nationality = models.CharField(max_length=100, default='Filipino')
+    citizenship = models.CharField(max_length=100, default='Filipino')
+    present_address = models.CharField(max_length=100, default='House No., Street Address, City, State')
+    permanent_address = models.CharField(max_length=100, default='House No., Street Address, City, State')
+    billing_address = models.CharField(max_length=100, default='House No., Street Address, City, State')
+    shopping_address = models.CharField(max_length=100, default='House No., Street Address, City, State')
+    office_address = models.CharField(max_length=100, default='House No., Street Address, City, State')
+    mobile_number = models.CharField(max_length=100, default='09xx xxx xxxx')
+    landline_number = models.CharField(max_length=100, default='09xx xxx xxxx')
+    office_number = models.CharField(max_length=100, default='09xx xxx xxxx')
+    is_subscribed = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.user.username} Profile'
