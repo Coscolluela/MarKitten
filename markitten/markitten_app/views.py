95de80c0c00e9baabf51d28172fef11220c23088
Romaine Dara Regala <rmregala@up.edu.ph>
2022-05-23 21:48:13 +0800

    Sign up Feature

--- markitten/markitten_app/views.py
+++ markitten/markitten_app/views.py
@@ -1,6 +1,16 @@
+import email
+from django.shortcuts import render,redirect
+from django.urls import reverse_lazy
+from .models import *
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import PasswordChangeView
+from .forms import *
 from django.shortcuts import render
 
 # Create your views here.
+@login_required(login_url='/login')
 def home(request):
     return render(request, 'markitten_app/Home.html')
 
@@ -21,10 +31,36 @@ def leavereview(request):
     return render(request, 'markitten_app/leaveReview.html')
 
 def signup(request):
-    return render(request, 'markitten_app/signup.html')
+    form = UserForm()
 
-def login(request):
+    if(request.method == "POST"):
+        form = UserForm(request.POST)
+        if(form.is_valid()):
+            form.save()
+            messages.success(request, "Account was created for " + form.cleaned_data.get("username"))
+            return redirect('/')
+        
+    data = {"form" : form}
+    return render(request, 'markitten_app/signup.html', data)
+
+def signin(request):
+    if(request.method == "POST"):
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(request, email = email, password = password)
+        if user is not None:
+            login(request, user)
+            print("Login was successful")
+            return redirect('/')
+        else:
+            print("Login failed")
+            messages.error(request, "Incorrect password or email.")
     return render(request, 'markitten_app/login.html')
+
+def signout(request):
+    logout(request)
+    return redirect('login')
 
 def faq(request):
     return render(request, 'markitten_app/faq.html')
