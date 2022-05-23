7788d17e3c36a9b6178a1c21d27c4ad0d73bf68f
Federico <jpcoscolluela@up.edu.ph>
2022-05-23 10:56:28 +0800

    Superuser + Edit Profile Adjustments

--- markitten/markitten/settings.py
+++ markitten/markitten/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'markitten.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'project128.1',
+        'HOST': 'database-128-1.cfhfs8afwgzf.us-east-2.rds.amazonaws.com',
+        'POST': '5432',
     }
 }
 
