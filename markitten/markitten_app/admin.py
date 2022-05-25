12f36632e1babee77f1ede428c6596d85f4f2999
Federico <jpcoscolluela@up.edu.ph>
2022-05-25 12:49:34 +0800

    Admin Filter

--- markitten/markitten_app/admin.py
+++ markitten/markitten_app/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Profile
+from .models import *
 
 admin.site.site_header = 'Markitten Admin Dashboard'
 
+class Filter(admin.ModelAdmin):
+    list_display = ("user", "is_subscribed")
+    list_filter = ("sex", "nationality", "birthday", "permanent_address")
+
 # Register your models here.
-admin.site.register(Profile)
+admin.site.register(Supplier)
+admin.site.register(Profile, Filter)
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Complaint)
